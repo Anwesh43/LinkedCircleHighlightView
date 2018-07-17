dcdaa341d20cc9ea2a26a4c101205df99524de47
Anwesh43 <anweshthecool0@gmail.com>
2018-07-17 09:29:10 +0530

    using Renderer in View

--- lchview/src/main/java/com/anwesh/uiprojects/lchview/LCHView.kt
+++ lchview/src/main/java/com/anwesh/uiprojects/lchview/LCHView.kt
@@ -48,16 +48,18 @@ fun Canvas.drawNode(i : Int, scale : Float, paint : Paint) {
 
 class LCHView(ctx : Context) : View(ctx) {
 
+    private val renderer : Renderer = Renderer(this)
+
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
