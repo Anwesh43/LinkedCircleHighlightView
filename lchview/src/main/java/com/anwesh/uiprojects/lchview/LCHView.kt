0afbd02a477ad1474a1ab3647c33d3e3055eb1bd
Anwesh43 <anweshthecool0@gmail.com>
2018-07-17 08:31:07 +0530

    importing libraries and adding nodes

--- lchview/src/main/java/com/anwesh/uiprojects/lchview/LCHView.kt
+++ lchview/src/main/java/com/anwesh/uiprojects/lchview/LCHView.kt
@@ -4,3 +4,11 @@ package com.anwesh.uiprojects.lchview
  * Created by anweshmishra on 17/07/18.
  */
 
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+val nodes : Int = 5
